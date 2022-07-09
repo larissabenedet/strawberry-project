93abc8b961d0633b5c502273281d5956c2227975
larissabenedet <larissavb2014@outlook.com>
2022-07-09 00:32:18 -0300

    feat: add global styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,12 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
- 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: 'Lato', sans-serif, Helvetica, Arial;
     box-sizing: border-box;
+    font-size: 100%;
+    color: #212121;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
 `;
- 
 export default GlobalStyle;
